b7d2c12d44134a64434d0fd829cfdd74b1d2a2aa
Meuons <89832844+Meuons@users.noreply.github.com>
2021-10-07 12:58:07 +0200

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+class database{
+//Store the database connection as properties
+
+private $host = 'studentmysql.miun.se';
+private $db_name ='mama2006';
+private $username = 'mama2006';
+private $password = 'mx4mtCZ2kR';
+private $conn;
+public function connect(){
+
+
+    
+
+    //Create a database connection
+    $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name );
+
+
+      if($this->conn->connect_errno > 0){
+        die("Fel vid anslutning: " . $this->conn->connect_error);
+    }
+
+    return $this->conn;
+
+}
+    public function close(){
+        $this->conn = null;
+    }
+}
+
